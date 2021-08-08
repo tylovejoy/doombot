740d1d89761e962130c383f28140ebf77b22c3e6
nebulaOW <religiossdoge@gmail.com>
2021-08-08 18:17:21 -0500

    fix: bugs

--- cogs/suggestions.py
+++ cogs/suggestions.py
@@ -65,7 +65,7 @@ class Suggestions(commands.Cog, name="Suggestions"):
         entry.reacted = entry.reacted + [payload.user_id]
         await entry.commit()
 
-        if entry.stars < 9:
+        if entry.stars < 5:
             return
 
         message: discord.Message = await self.suggestion_channel.get_partial_message(
