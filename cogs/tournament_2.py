b3d07815d22b7ce21aff56661b34c70c1431ea77
Ty <ty@fromskytocenter.com>
2021-11-04 08:18:44 -0500

    add maps/missions support

--- cogs/tournament_2.py
+++ cogs/tournament_2.py
@@ -27,41 +27,58 @@ else:
 logger = getLogger(__name__)
 
 
-async def _setup_db(name, start, end, embed):
-    last_tournament = await TournamentData().find_one(sort=[("tournament_id", -1)], limit=1)
-    if last_tournament:
-        tournament_id = last_tournament.tournament_id + 1
-    else:
-        tournament_id = 1
-
-    tournament = TournamentData(
-        **{
-            "tournament_id": tournament_id,
-            "name": name,
-            "schedule_start": start,
-            "schedule_end": end,
-            "embed_dict": embed,
-        }
-    )
-    tournament.records = TournamentRecords()
-    tournament.records = {
-        "ta": [],
-        "mc": [],
-        "hc": [],
-        "bo": [],
-    }
-    await tournament.commit()
-
-
 class Tournament2(commands.Cog, name="Tournament2"):
     """Tournament2"""
 
     def __init__(self, bot):
         self.bot = bot
+        self.cur_tournament = None
+
+    async def _setup_db(self, name, start, end, maps, embed):
+        if self.cur_tournament:
+            # TODO: Dont allow multiple tournaments.
+            pass
+
+        last_tournament = await TournamentData().find_one(sort=[("tournament_id", -1)], limit=1)
+        if last_tournament:
+            tournament_id = last_tournament.tournament_id + 1
+        else:
+            tournament_id = 1
+        self.cur_tournament = tournament_id
+
+        tournament = TournamentData(
+            **{
+                "tournament_id": tournament_id,
+                "name": name,
+                "schedule_start": start,
+                "schedule_end": end,
+                "embed_dict": embed,
+            }
+        )
+        tournament.maps = {**maps}
+        tournament.records = {
+            "ta": [],
+            "mc": [],
+            "hc": [],
+            "bo": [],
+        }
+        tournament.missions = {
+            "ta": [],
+            "mc": [],
+            "hc": [],
+            "bo": [],
+        }
+        await tournament.commit()
 
     @commands.command()
     async def test(self, ctx):
-        logger.info(await _setup_db("test", "1", "1", {}))
+        maps = {
+            "ta": {"code": "CODE", "map": "MAP", "author": "AUTHOR"},
+            "mc": {"code": "CODE", "map": "MAP", "author": "AUTHOR"},
+            "hc": {"code": "CODE", "map": "MAP", "author": "AUTHOR"},
+            "bo": {"code": "CODE", "map": "MAP", "author": "AUTHOR"},
+        }
+        logger.info(await self._setup_db("test", "1", "1", maps, {}))
 
 
 
