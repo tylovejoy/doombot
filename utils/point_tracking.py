4266ae7dae8e9201616554f451617c0459a8d5ee
Ty <ty@fromskytocenter.com>
2021-11-09 08:17:39 -0600

    added point tracking

---
+++ utils/point_tracking.py
@@ -0,0 +1,21 @@
+class PointTracking:
+
+    def __init__(self, records):
+        self.records = records
+        self._top = records[0]["record"]
+        self._min = self._top * 1.2
+        self._points = {}
+
+    def compute_points(self):
+        for record in self.records:
+            if record.record > 0:
+                points = 2500
+                if record.record > self._min:
+                    points -= 2400
+                else:
+                    points -= ((record.record - self._top) * (2400 / (self._min - self._top)))
+            else:
+                points = 0
+
+            self._points[record.posted_by] = points
+
